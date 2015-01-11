f99fe36b2958f6b96528abb13c7a1c03c9ce26c2
Bhupendra2020 <bhupendra.kumar@softcrylic.com>
2015-01-11 03:03:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-if [ ! -f /data/db/mongod.lock ]; then
-    echo "Finding mongod.lock file !!"
-    eval $mongodb
-else
-    export mongodb=$mongodb' --dbpath /data/db' 
-    rm /data/db/mongod.lock
-    echo "remove mongod.lock file !!"
-    mongod --dbpath /data/db --repair && eval $mongodb
-fi
+
 
 if [ ! -f /.mongodb_password_set ]; then
 	/set_mongodb_password.sh
@@ -17,6 +9,16 @@ if [ "$AUTH" == "yes" ]; then
     export mongodb='/usr/bin/mongod --nojournal --auth --httpinterface --rest'
 else
     export mongodb='/usr/bin/mongod --nojournal --httpinterface --rest'
+fi
+
+if [ ! -f /data/db/mongod.lock ]; then
+    echo "Finding mongod.lock file !!"
+    eval $mongodb
+else
+    export mongodb=$mongodb' --dbpath /data/db' 
+    rm /data/db/mongod.lock
+    echo "remove mongod.lock file !!"
+    mongod --dbpath /data/db --repair && eval $mongodb
 fi
 
 
